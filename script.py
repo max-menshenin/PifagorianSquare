33b23dee474eae8f4a785bef1b3118f303bb188f
Postmind <53375202+max-menshenin@users.noreply.github.com>
2023-01-05 21:51:25 +0300

    added third_base_number and fourth

--- script.py
+++ script.py
@@ -19,6 +19,11 @@ birth_year = 1991
 first_base_number = sum_of_digits(birth_year) + sum_of_digits(birth_month) + sum_of_digits(birth_day)
 second_base_number = sum_of_digits(first_base_number)
 third_base_number = first_base_number - 2 * first_number(birth_day)
+fourth_base_number = sum_of_digits(third_base_number)
 
 print(first_base_number)
+print(second_base_number)
 print(third_base_number)
+print(fourth_base_number)
+
+
