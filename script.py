e44a3804a4574c04425cc1dad8002d82a9b65038
Postmind <53375202+max-menshenin@users.noreply.github.com>
2023-01-05 19:02:24 +0300

    Initial commit

---
+++ script.py
@@ -0,0 +1,24 @@
+def sum_of_digits(num):
+    sum = 0
+    while num > 0:
+        sum += num % 10
+        num //= 10
+    return sum
+
+
+def first_number(num):
+    while num >= 10:
+        num = num // 10
+    return int(num)
+
+
+birth_day = 24
+birth_month = 3
+birth_year = 1991
+
+first_base_number = sum_of_digits(birth_year) + sum_of_digits(birth_month) + sum_of_digits(birth_day)
+second_base_number = sum_of_digits(first_base_number)
+third_base_number = first_base_number - 2 * first_number(birth_year)
+
+print(first_base_number)
+print(sum_of_digits(first_base_number))
